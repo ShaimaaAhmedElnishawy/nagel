ed291b7b316db55b4c325d4c03809dab14cc22df
shaimaa <shaimaaahmed392003@gmail.com>
2025-05-03 23:11:56 +0200

    .

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -47,7 +47,7 @@ class PatientController extends BaseController
             'file', // The key for the file
             $imageContent, // The file content
             $imageFile->getClientOriginalName() // The file name
-        )->post('https://0a84-41-43-224-226.ngrok-free.app/predict/');
+        )->post('https://shaky-bushes-dream.loca.lt/predict/');
         
         // Check if the request was successful
         if ($response->successful()) {
